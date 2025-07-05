803d0335f50a5b8da7d917ef5b0816a4f7524a16
fad <ojy9406@naver.com>
2025-07-05 18:21:50 +0900

    init.

---
+++ src/main/java/com/footsallife/fc/FootsallifeFcApplication.java
@@ -0,0 +1,12 @@
+package com.footsallife.fc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootsallifeFcApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FootsallifeFcApplication.class, args);
+	}
+
+}
